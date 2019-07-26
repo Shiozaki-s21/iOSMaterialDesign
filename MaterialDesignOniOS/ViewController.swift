d37829d415806ee4cf695f14598fa04971c8656a
Kazuya <shiozaki5621@gmail.com>
2019-07-25 20:40:50 -0700

    add MaterialBottomNavigationBar

--- MaterialDesignOniOS/ViewController.swift
+++ MaterialDesignOniOS/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
+// Material Components
+import MaterialComponents.MaterialBottomNavigation
 
 class ViewController: UIViewController {
 
+    let bottomNavBar:MDCBottomNavigationBar = MDCBottomNavigationBar()
+
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+        super.viewDidLoad()
+
+        createBottomNavigationBar()
+        view.addSubview(bottomNavBar)
+
     }
 
+    private func createBottomNavigationBar() {
+        bottomNavBar.sizeThatFitsIncludesSafeArea = false
+        let size = bottomNavBar.sizeThatFits(view.bounds.size)
+        var bottomNavBarFrame = CGRect(x: 0, y: view.bounds.height - size.height, width: size.width, height: size.height)
+        if #available(iOS 11.0, *) {
+            bottomNavBarFrame.size.height += view.safeAreaInsets.bottom
+            bottomNavBarFrame.origin.y -= view.safeAreaInsets.bottom
+        }
 
+        bottomNavBar.frame = bottomNavBarFrame
 
+        bottomNavBar.acc
+    }
 }
